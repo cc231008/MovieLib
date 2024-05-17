89ed0b5c67779e082ad5589caf1e3487aaf60cc1
cc231008 <cc231008@fhstp.ac.at>
2024-05-17 13:07:11 +0200

    console.log(data about movies)

--- app/movies/movies.jsx
+++ app/movies/movies.jsx
@@ -1,11 +1,17 @@
-export default function Movies() {
+'use client';
+import React, { useEffect } from 'react';
+export default function MoviesDB() {
     const getMovies = async () => {
         const response = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=191cc9b801df0a2851e72996f511f57d');
         const data = await response.json();
+        console.log(data.results);
     }
+    useEffect(() => {
+        getMovies();
+    }, []);
     return (
         <div>
-        <h1>Movies</h1>
+            <h1>Movies</h1>
         </div>
     );
 }
---
+++ app/movies/page.js
@@ -0,0 +1,9 @@
+import MoviesDB from "@/app/movies/movies";
+
+export default function Movies() {
+    return (
+        <div>
+            <MoviesDB />
+        </div>
+    );
+}
