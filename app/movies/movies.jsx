a10e9f4cfec419403abc8d4831f571855465809c
cc231008 <cc231008@fhstp.ac.at>
2024-05-17 17:47:10 +0200

    console.log(data about movies)

--- app/movies/movies.jsx
+++ app/movies/movies.jsx
@@ -1,17 +1,32 @@
 'use client';
 import React, { useEffect } from 'react';
+import { useState } from 'react';
 export default function MoviesDB() {
+    const [movies, setMovies] = useState([]);
+
+    const API_URL = 'https://api.themoviedb.org/3/discover/movie?api_key=191cc9b801df0a2851e72996f511f57d'
+
     const getMovies = async () => {
-        const response = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=191cc9b801df0a2851e72996f511f57d');
+        const response = await fetch(`${API_URL}`);
         const data = await response.json();
-        console.log(data.results);
+        setMovies(data.results);
     }
     useEffect(() => {
         getMovies();
     }, []);
+
+    console.log(movies);
+
     return (
         <div>
-            <h1>Movies</h1>
+            <div className="container">
+                {movies.map((movie) => (
+                    <div key={movie.id}>
+                        <h3>{movie.title}</h3>
+                        <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
