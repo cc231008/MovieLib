1adfcd07a09f461d4754602580e58837ba7304a0
cc231008 <cc231008@fhstp.ac.at>
2024-06-02 15:15:32 +0200

    .env was added to .gitignore

--- app/tmdb/layout.js
+++ app/tmdb/layout.js
@@ -1,5 +1,4 @@
 import Header from "./components/Header";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 require('dotenv').config()
 
 export default function TMDBLayout({ children }) {
